774933bc1cbf6f039ac0d86a0ba96cbeb31cf723
Edson Bruno <135662753+sucloudflare@users.noreply.github.com>
2024-04-09 15:06:11 -0300

    Create soma-subtracao.java

---
+++ desafio-1/soma-subtracao.java
@@ -0,0 +1,14 @@
+public class Soma{
+
+    public static void main(String[] args) {
+        System.out.println(10 + 5);
+    }
+}
+
+public class Subtração{
+
+    public static void main(String[] args) {
+        System.out.println(10 - 5);
+    }
+}
+
