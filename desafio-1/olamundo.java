fd10660a82c9234533c7a1d37e928f6ffe1ffb3f
Edson Bruno <135662753+sucloudflare@users.noreply.github.com>
2024-04-09 15:05:12 -0300

    Create olamundo.java

---
+++ desafio-1/olamundo.java
@@ -0,0 +1,30 @@
+public class Perfil{
+
+    public static void main(String[] args) {
+        System.out.println("Olá, Nome");
+    }
+
+}  
+
+
+public class Perfil{
+
+    public static void main(String[] args) {
+        System.out.println("Olá, Nome");
+        System.out.println("Tudo bem?");
+    }
+
+}
+
+
+public class Estudos{
+
+    public static void main(String[] args) {
+        System.out.println("JVM (Java Virtual Machine) - é a Máquina Virtual do Java responsável por executar o bytecode");
+        System.out.println("JRE (Java Runtime Environment) - Ambiente de Execução do Java que fornece as bibliotecas padrões do Java para o JDK compilar o seu código e para a JVM executar o seu program");
+    }
+
+}
+
+
+
